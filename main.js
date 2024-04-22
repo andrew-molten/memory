c3f1eea369e2fbf9d4e9a2135d8b5994fae36ab1
Andrew Molten <andrewmolten23@gmail.com>
2024-04-22 12:44:32 +1200

    add pseudocode and start coding

---
+++ main.js
@@ -0,0 +1,11 @@
+// add a list of possible tiles
+
+// create a tile object
+
+// choose the number of tiles for the game
+
+// assign tiles to random positions on the screen
+
+// check for a match & remove tile/ do a celebration
+
+// count matches
