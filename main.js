a136505df91301c165f0514bda0b602d8b56a710
Andrew Molten <andrewmolten23@gmail.com>
2024-04-22 14:52:18 +1200

    add images to tiles array

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // add a list of possible tiles
-const tiles = [{}]
+const tiles = [{url: images/memory/alberto.jpg}, {url:images/memory/anna-ute.jpg}, {url:images/memory/anna.jpg}, {url:images/memory/bingo.jpg}, {url:images/memory/bluey.jpg}, {url:images/memory/gran-mala.jpg}, {url:images/memory/gran.jpg}, {url:images/memory/luca.jpg}, {url:images/memory/mala-andrew-crumpets.jpg}, {url:images/memory/mala-emmie.jpg}, {url:images/memory/mala-emmy.jpg}, {url:images/memory/mala-imogen.jpg}, {url:images/memory/mala-kiba.jpg}, {url:images/memory/mala-kiba2.jpg}, {url:images/memory/mala-pony.jpg}, {url:images/memory/mala-thomas-climbing.jpg}, {url:images/memory/mala-thomas.jpg}, {url:images/memory/mama-papa.jpg}, {url:images/memory/moana.jpg}, {url:images/memory/monkey-tongue.jpg}, {url:/home/andrew/devacademy/personal-projects/memory/images/memory/oma-eis.jpg}, {url:images/memory/oma-tilo.jpg}, {url:images/memory/omi-mala.jpg}, {url:images/memory/opa-dressup.jpg}, {url:images/memory/sheep.jpg}, {url:images/memory/tilo.jpg}, {url:images/memory/triceratops.png}]
 
 // create a tile object
 
